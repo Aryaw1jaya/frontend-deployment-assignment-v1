24ce8790d1b5967c048bd2fa27d1c51208529f31
Arya Wijaya <aryaw1555@gmail.com>
2022-12-20 03:15:24 +0700

    Initial commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Arya Wijaya"; // TODO: replace this
+const studentId = "FE2628549"; // TODO: replace this
+
+export { url, studentName, studentId };
