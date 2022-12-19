aa4e8cb1eb4bcf4639e4fc686c50d37b98c745f3
Arya Wijaya <aryaw1555@gmail.com>
2022-12-20 04:09:52 +0700

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL"; // TODO: replace this
+const url = "https://main--golden-bienenstitch-9690a5.netlify.app/"; // TODO: replace this
 const studentName = "Arya Wijaya"; // TODO: replace this
 const studentId = "FE2628549"; // TODO: replace this
 
